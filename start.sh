93f4d2b600d6f23ac2516a33582ebac921aecd64
ludavilla <30967356+ludavilla@users.noreply.github.com>
2024-09-15 21:02:09 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,6 +20,9 @@ terraform output |grep -i IP_floating |cut -d "=" -f2 > hosts1
 echo "Criando grupo de IPs com nome de nerday e inserindo ip da instancia no arquivo hosts"
 echo "[nerdday]" > hosts
 sed 's/"//g' hosts1 >> hosts
+echo " " >> hosts
+echo "[nerdday:vars]" >> hosts
+echo "ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> hosts
 rm -f hosts1
 
 ansible-playbook -i hosts main.yml -v
